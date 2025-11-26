9c6411660070f64b4177ad4d9536f61c4562d8c0
Vinayak Dubey <88390255+VinayakDubey07@users.noreply.github.com>
2025-11-26 11:33:43 +0000

    commiting

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import { ColorModeScript } from '@chakra-ui/react'
 import React, { StrictMode } from 'react'
 import App from './App'
 
-
 import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
